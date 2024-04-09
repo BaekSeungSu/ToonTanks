f7ff86ab0bb2f6de9e06e4e7b2bcc6f20430efb7
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-04-09 17:48:57 +0900

    Forward Declaration

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -19,6 +19,10 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:
+	UPROPERTY()
+	class UCapsuleComponent* CapsuleComp;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
