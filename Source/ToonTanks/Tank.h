9c1a60a6a26114ba3bea694ab44353ac3a489851
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-04-12 06:07:07 +0900

    Tank 클래스 추가
    BasePawn클래스를 상속받은 Tank클래스 추가, 카메라 추가

---
+++ Source/ToonTanks/Tank.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tank.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class TOONTANKS_API ATank : public ABasePawn
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class USpringArmComponent *SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class UCameraComponent *Camera;
+
+public:
+	ATank();
+};
