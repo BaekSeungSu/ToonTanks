442ca400ae3ed6ebd04b7da36b2199a061e65239
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-04-13 22:59:01 +0900

    Possession
    set Auto Possess Player to Player0

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Components/CapsuleComponent.h"
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
