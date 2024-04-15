ac2671efb3b1e2dca551020d3d1c911cafb9866c
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-04-15 23:30:09 +0900

    Tower 회전
    사정거리 안에 들어오면 플레이어를 바라보도록 작성

---
+++ Source/ToonTanks/Tower.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tower.h"
+#include "Tank.h"
+#include "Kismet/GameplayStatics.h"
+
+void ATower::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+void ATower::Tick(float Deltatime)
+{
+    Super::Tick(Deltatime);
+
+    if(Tank){
+        // 탱크까지의 거리 구하기
+        float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+        // 탱크가 사정거리에 있는지 확인
+        if(Distance <= FireRange){
+            // 사정거리에 있으면 포탑을 탱크를 향해 회전
+            RotateTurret(Tank->GetActorLocation());
+        }
+    }
+
+}
---
+++ Source/ToonTanks/Tower.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tower.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class TOONTANKS_API ATower : public ABasePawn
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Tick(float Deltatime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	class ATank* Tank;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float FireRange = 200.f;
+};
