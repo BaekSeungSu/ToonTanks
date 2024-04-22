6340a08c7d178c2d9e1ae991cc84b3ff941e016f
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-04-22 15:01:20 +0900

    사용자 지정 플레이어 컨트롤러 생성

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -5,6 +5,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "Tank.h"
 #include "Tower.h"
+#include "ToonTanksPlayerController.h"
 
 void AToonTanksGameMode::ActorDied(AActor* DeadActor)
 {
@@ -12,9 +13,8 @@ void AToonTanksGameMode::ActorDied(AActor* DeadActor)
     {
         Tank->HandleDestruction();
 
-        if(Tank->GetTankPlayerController()){
-            Tank->DisableInput(Tank->GetTankPlayerController());
-            Tank->GetTankPlayerController()->bShowMouseCursor = false;
+        if(ToonTanksPlayerController){
+            ToonTanksPlayerController->SetPlayerEnabledState(false);
         }
     }
     else if(ATower* DestroyedTower = Cast<ATower>(DeadActor)){
@@ -27,4 +27,5 @@ void AToonTanksGameMode::BeginPlay()
     Super::BeginPlay();
     
     Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+    ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
 }
--- Source/ToonTanks/ToonTanksGameMode.h
+++ Source/ToonTanks/ToonTanksGameMode.h
@@ -23,4 +23,5 @@ protected:
 
 private:
 	class ATank* Tank;
+	class AToonTanksPlayerController* ToonTanksPlayerController;
 };
